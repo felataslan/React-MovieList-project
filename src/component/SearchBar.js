38eaafc4ad575b0f294f07d198a6f72e9627ad4e
felat aslan <felataslan58@gmail.com>
2022-10-03 15:55:33 +0300

    first commit

---
+++ src/component/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+    render() {
+        return (
+            <h1>
+                <input  className="w-100 mt-4" type={'text'}/>
+            </h1>
+        );
+    }
+}
+export default SearchBar;
