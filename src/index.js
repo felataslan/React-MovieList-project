c1dababce2c9bd92b6b84a50229fd82d59d34098
felat aslan <felataslan58@gmail.com>
2022-10-08 19:24:59 +0300

    react-web-project My-Movies project is done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './component/App';
-import AddMovie from './component/AddMovie';
+
+import App from './components/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import {
-    BrowserRouter,
-    Routes,
-    Route,
-  } from "react-router-dom"
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="/AddMovie" element={<AddMovie/>} />
-      
-      </Routes>
-    </BrowserRouter>
-  );
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
